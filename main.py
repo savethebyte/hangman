dee4767aa79d9ac1ee43400a7e9a917888c18170
savethebyte <jake@savethebyte.com>
2019-07-04 22:46:02 -0500

    cleanup output

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def main():
 #Function to print letters and number of guesses remaining
 def computeWord(secret, letter, reveal, guesses):
     if len(letter) == 1 and letter.isalpha():
-        print("'{0}' appears {1} time/s in the word!".format(letter,secret.count(letter)))
+        print("'{0}' appears {1} time/s in the word!".format(letter, secret.count(letter)))
         for char in range(0,len(secret)):
             if secret[char] == letter:
                 reveal[char] = letter
